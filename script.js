4573df9c3a14957e5ee3f33d3b4eb23bf94b62aa
Denis <hallo-boy@mail.ru>
2022-10-05 11:10:24 +0300

    html and css work

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const input = document.querySelector(".search");
 
-const url =
-  "https://api.unsplash.com/search/photos?query=" +
-  input.value +
-  "&per_page=2&client_id=34JNzm_MQs2Fqtbk8rnqv-w1uTg8A5lYDugu9rQgiEE";
-fetch(url)
-.then((response) => {
-  console.log(response);
-});
+
+
+apiRequest = () => {
+  const url =
+    "https://api.unsplash.com/search/photos?query=" +
+    input.value +
+    "&per_page=15&client_id=gzW7SclKMoE2twTl7ftCBpV9kfUNTXpxP9UTGS1i1kI";
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+
+    .then((data) => {
+      loadImages(data);
+    })
+
+    .catch((error) => console.log(error));
+};
+
+const loadImages = (data) => {
+for ( let i = 0; i < data.results.length; i++)
+{
+  let image = document.createElement("div");
+    image.className = "img";
+    image.style.backgroundImage =  "url("+data.results[i].urls.raw + "&w=1366&h=768"+")";
+    document.querySelector('.container').appendChild(image);
+}
+}
+apiRequest()
